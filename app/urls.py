a998b224093811c70087b693252b4a28c95f2366
Adnan Ashraf <adnanashraf4423@gmail.com>
2024-03-19 21:03:32 +0500

    Updated

--- app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import TemplateListCreateView, WebsiteListCreateView, PageListCreateView, WebsiteDetailAPIView
-
-urlpatterns = [
-    path('templates/', TemplateListCreateView.as_view(), name='template-list'),
-    path('websites/', WebsiteListCreateView.as_view(), name='website-list'),
-    path('websites/<int:pk>/', WebsiteDetailAPIView.as_view(), name='website-detail'),
-    path('pages/', PageListCreateView.as_view(), name='page-list'),
-]
